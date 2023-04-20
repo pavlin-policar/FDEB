f2ef15135cf41ac2897faf2c6789f46eac87fd8e
Pavlin Poličar <pavlin.g.p@gmail.com>
2023-04-20 11:01:00 +0200

    Implement FDEB without compatibility measures

---
+++ fdeb/fdeb.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+
+def interleave(x0, x1):
+    """Interleave two arrays along the first axis."""
+    assert x0.shape[1:] == x1.shape[1:], "Incompatible shapes!"
+
+    y = np.empty((x0.shape[0] + x1.shape[0], *x0.shape[1:]), dtype=x0.dtype)
+    y[::2] = x0
+    y[1::2] = x1
+    return y
+
+
+def subdivide_edge(e, axis=1):
+    e = np.swapaxes(e, 0, axis)
+    midpoints = np.array([(e0 + e1) / 2 for e0, e1 in zip(e[1:], e[:-1])])
+    e = interleave(e, midpoints)
+    e = np.swapaxes(e, axis, 0)
+    return e
+
+
+def compute_forces(e, kp):
+    # Left-mid spring direction
+    v_spring_l = e[:, :-1, :] - e[:, 1:, :]
+    v_spring_l = np.concatenate(
+        [np.zeros((v_spring_l.shape[0], 1, v_spring_l.shape[-1])), v_spring_l],
+        axis=1,
+    )
+
+    # Right-mid spring direction
+    v_spring_r = e[:, 1:, :] - e[:, :-1, :]
+    v_spring_r = np.concatenate(
+        [v_spring_r, np.zeros((v_spring_l.shape[0], 1, v_spring_l.shape[-1]))],
+        axis=1,
+    )
+
+    f_spring_l = np.sum(v_spring_l ** 2, axis=-1, keepdims=True)
+    f_spring_r = np.sum(v_spring_r ** 2, axis=-1, keepdims=True)
+
+    F_spring = kp * (f_spring_l * v_spring_l + f_spring_r * v_spring_r)
+
+    # Electrostatic force
+    v_electro = np.expand_dims(e, 1) - np.expand_dims(e, 0)
+    f_electro = 1 / (np.sqrt(np.sum(v_electro ** 2, axis=-1, keepdims=True)) + 1e-8)
+
+    F_electro = np.sum(f_electro * v_electro, axis=0)
+
+    F = F_spring + F_electro
+    # The first and last points are fixed
+    F[:, 0, :] = F[:, -1, :] = 0
+
+    return F
+
+
+def fdeb(edges, K=0.1, n_iter=50, n_iter_reduction=2 / 3, lr=0.04, lr_reduction=0.5, num_cycles=6):
+    initial_edge_lengths = np.sum((edges[:, 0] - edges[:, -1]) ** 2, axis=1)
+    # Add a small value to avoid division by zero
+    initial_edge_lengths += 1
+
+    for cycle in range(num_cycles):
+        edges = subdivide_edge(edges)
+
+        num_segments = edges.shape[1] - 1
+        kp = K / initial_edge_lengths * num_segments
+        kp = np.expand_dims(kp, (1, 2))
+
+        for epoch in range(n_iter):
+            F = compute_forces(edges, kp)
+            edges += F * lr
+
+        n_iter = int(np.ceil(n_iter * n_iter_reduction))
+        lr = lr * lr_reduction
+
+    return edges
